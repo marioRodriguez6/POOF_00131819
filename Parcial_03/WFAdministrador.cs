5d846d971f4c8a8700e147649bcc7f56b2a01816
marioR <mariojosue155@gmail.com>
2020-07-02 12:54:33 -0600

    First commit Working alone :(

---
+++ Parcial_03/WFAdministrador.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace Parcial_03
+{
+    public partial class WFAdministrador : Form
+    {
+        public WFAdministrador()
+        {
+            InitializeComponent();
+        }
+    }
+}
